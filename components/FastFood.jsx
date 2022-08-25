607571be38c4a820895b2bc4ca4222f42b7d6614
Jewel Osafile <osafilejewel@gmail.com>
2022-08-25 22:40:06 +0100

    tabs

---
+++ components/FastFood.jsx
@@ -0,0 +1,29 @@
+import { View, Text, Image, TouchableOpacity} from 'react-native'
+import React from 'react'
+import avatar from '../assets/fast1.png'
+import {BookmarkIcon as OutlinedBookmark} from 'react-native-heroicons/outline'
+import {BookmarkIcon as SolidBookmark, PlusIcon} from 'react-native-heroicons/solid'
+const FastFood = () => {
+  return (
+        <TouchableOpacity className="h-48 w-36 mt-3 p-2 relative  bg-transparent  rounded-md border border-gray-400 ">
+            <TouchableOpacity className="rounded p-2  items-center self-end  bg-lightishPink">
+                {/* <SolidBookmark size={20} color="#de1f27" /> */}
+                <OutlinedBookmark size={18} color="#fff" style={{fontWeight: 'bold'}} />
+            </TouchableOpacity>
+            <Image source={avatar} className="w-20 mt-8 absolute  self-center h-20" />
+           <View className="flex mt-14">
+            <Text className="text-md font-bold mt-3 text-md" >Fast Food</Text>
+                <Text className=" text-gray-400 text-xs font-bold">Fast Food</Text>
+            <View className="flex flex-row mt-1 justify-between">
+            <Text className="text-lg font-bold">₦5,000</Text>
+                <TouchableOpacity className="bg-darkRed items-center p-2 rounded-md">
+                    <PlusIcon size={16} color="#fff" />
+                </TouchableOpacity>
+            </View>
+            </View>
+        </TouchableOpacity>
+        
+  )
+}
+
+export default FastFood
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,12 +1,18 @@
-import { SafeAreaView, View, Text, Image, TouchableOpacity, TextInput } from 'react-native'
-import React, {useLayoutEffect} from 'react'
+import { SafeAreaView, View, Text, Image, TouchableOpacity, TextInput, ScrollView } from 'react-native'
+import React, {useLayoutEffect, useState} from 'react'
 import {useNavigation} from '@react-navigation/native'
 import GlobalStyles from '../GlobalStyles'
 import {Bars3CenterLeftIcon, MagnifyingGlassIcon} from 'react-native-heroicons/outline'
 import avatar from '../assets/avatar1.png' 
+import Fruit from '../components/Fruit'
+import Vegetable from '../components/Vegetable'
+import FastFood from '../components/FastFood'
+import Bakery from '../components/Bakery'
+
 
 const HomeScreen = () => {
     const navigation = useNavigation()
+    const [ defaultTab, switchTab ] = useState(0);
 
     useLayoutEffect(() => (
         navigation.setOptions({
@@ -20,7 +26,7 @@ const HomeScreen = () => {
             <TouchableOpacity className="bg-lightishPink rounded p-2">
                 <Bars3CenterLeftIcon size={25} style={{fontWeight: 'bold'}} color="#de1f27" />
             </TouchableOpacity>
-            <Text className="text-xl text-center font-bold">Home</Text>
+            <Text className="text-2xl text-center font-bold">Home</Text>
             <TouchableOpacity className="bg-lightishPink p-1 rounded">
                 <Image 
                     source={avatar}
@@ -28,13 +34,70 @@ const HomeScreen = () => {
                 />
             </TouchableOpacity>
       </View>
-      <Text className="text-xl mt-4  font-bold">Let's find best food here</Text>
-      <TouchableOpacity className="flex-row mt-4 bg-slate-200 p-3">
+      <Text className="text-xl mt-5  font-bold">Let's find best food here</Text>
+      <TouchableOpacity className="flex-row mt-2 bg-slate-200 p-3">
         <MagnifyingGlassIcon size={25} style={{fontWeight: 'bold'}} color="#ccc" />
         <Text className="ml-3 font-bold text-slate-400 mt-1">Search now...</Text>
       </TouchableOpacity>
 
-      
+        {/* categories */}
+      <View className="flex-row space-x-5 mt-5">
+        <TouchableOpacity onPress={() => switchTab(0)}>
+            <Text className={`text-lg text-slate-400 font-bold ${defaultTab === 0 &&  'text-slate-900'}`}>
+                Fruits
+            </Text>
+            {defaultTab == 0 && (<View className="w-1 h-1 bg-gray-900 rounded-full items-center self-center">
+
+            </View>)}
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => switchTab(1)}>
+            <Text className={`text-lg text-slate-400 font-bold ${defaultTab === 1 && 'text-slate-900'}`}>
+                Vegetable
+            </Text>
+            {defaultTab == 1 &&(<View className="w-1 h-1 bg-gray-900 rounded-full items-center self-center">
+
+            </View>)}
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => switchTab(2)}>
+            <Text className={`text-lg text-slate-400 font-bold ${defaultTab === 2 &&  'text-slate-900'}`}>
+                Bakery
+            </Text>
+            {defaultTab == 2 &&(<View className="w-1 h-1 bg-gray-900 rounded-full items-center self-center">
+
+            </View>)}
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => switchTab(3)}>
+            <Text className={`text-lg text-slate-400 font-bold ${defaultTab === 3 && 'text-slate-900'}`}>
+                Fast Food
+            </Text>
+            {defaultTab == 3 &&(<View className="w-1 h-1 bg-gray-900 rounded-full items-center self-center">
+
+            </View>)}
+        </TouchableOpacity>
+      </View>
+
+    <View>
+        {defaultTab === 0 && (
+            <ScrollView>
+                <Fruit />
+            </ScrollView>
+        )}
+        {defaultTab === 1 && (
+            <ScrollView>
+                <Vegetable />
+            </ScrollView>
+        )}
+        {defaultTab === 2 && (
+            <ScrollView>
+                <Bakery />
+            </ScrollView>
+        )}
+        {defaultTab === 3 && (
+            <ScrollView>
+            <FastFood />
+            </ScrollView>
+        )}
+    </View>
     </SafeAreaView>
     
   )
