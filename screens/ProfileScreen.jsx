013cbb53b7b92c970a89e9ef5e6bbb3bd4f3b9a0
Jewel Osafile <osafilejewel@gmail.com>
2022-08-26 12:10:00 +0100

    profile Screen

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,8 +1,10 @@
-import { View, SafeAreaView, TouchableOpacity, Text,  } from 'react-native'
+import { View, SafeAreaView, TouchableOpacity, Text, Image} from 'react-native'
 import React, {useLayoutEffect} from 'react'
 import GlobalStyles from '../GlobalStyles'
-import {ChevronLeftIcon, PencilSquareIcon} from 'react-native-heroicons/outline'
+import {ChevronLeftIcon, ClipboardDocumentIcon, PencilSquareIcon} from 'react-native-heroicons/outline'
 import { useNavigation } from '@react-navigation/native'
+import avatar from '../assets/avatar1.png'
+
 
 
 
@@ -26,9 +28,25 @@ const ProfileScreen = () => {
             </TouchableOpacity>
             
       </View>
-      <View>
-        
+      <View className="bg-lightishPink mt-16 items-center justify-center self-center h-32 w-32 rounded-3xl">
+        <Image source={avatar} className="w-28  h-28" />
       </View>
+      <Text className="text-xl font-semibold text-center mt-5">Michael Thomas</Text>
+      <Text className="text-md font-bold mt-2 text-gray-400 text-center">@Michael</Text>
+    
+        <View className="bg-lightishPink h-24 flex flex-row">
+            <View>
+                <ClipboardDocumentIcon size={16} color="#ccc" />
+                <Text>order</Text>
+            </View>
+            <View>
+                <Text>payment</Text>
+            </View>
+            <View>
+                <Text>location</Text>
+            </View>
+        </View>
+    
     </SafeAreaView>
   )
 }
