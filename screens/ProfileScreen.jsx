db8877d42ae40e0a9f63a7ed5d8dc1dc823c9842
Jewel Osafile <osafilejewel@gmail.com>
2022-08-26 21:59:42 +0100

    profile screen

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -7,7 +7,7 @@ import {ChevronLeftIcon, ChevronRightIcon, UserIcon, Cog6ToothIcon, ChatBubbleOv
 } from 'react-native-heroicons/outline'
 import { useNavigation } from '@react-navigation/native'
 import avatar from '../assets/avatar1.png'
-
+import ngn from '../assets/ngn.png'
 
 
 
@@ -26,17 +26,17 @@ const ProfileScreen = () => {
             <TouchableOpacity onPress={() => navigation.goBack()} className="bg-lightishPink rounded-lg p-2">
                 <ChevronLeftIcon size={25} style={{fontWeight: 'bold'}} color="#de1f27" />
             </TouchableOpacity>
-            <Text className="text-2xl text-center font-bold">Profile</Text>
+            <Text className="text-xl text-center font-bold">Profile</Text>
             <TouchableOpacity className="bg-lightishPink rounded-lg p-2">
                 <PencilSquareIcon size={25} style={{fontWeight: 'bold'}} color="#de1f27" />
             </TouchableOpacity>
             
       </View>
-      <View className="bg-lightishPink mt-16 items-center justify-center self-center h-32 w-32 rounded-3xl">
+      <View className="bg-lightishPink mt-14 items-center justify-center self-center h-32 w-32 rounded-3xl">
         <Image source={avatar} className="w-28  h-28" />
       </View>
-      <Text className="text-xl font-semibold text-center mt-5">Michael Thomas</Text>
-      <Text className="text-md font-bold mt-2 text-gray-400 text-center">@Michael</Text>
+      <Text className="text-xl font-semibold text-center mt-5">Jewel Sama</Text>
+      <Text className="text-md font-bold mt-2 text-gray-400 text-center">@Jewel</Text>
     
         <View className="bg-lightishPink mt-3 h-20 p-5 justify-between flex flex-row rounded-b-2xl">
             <TouchableOpacity className="flex flex-col items-center space-x-1">
@@ -78,14 +78,16 @@ const ProfileScreen = () => {
                     <LanguageIcon size={23} color="rgb(75, 85, 99)" />
                 </View>
                 <Text className="flex-1 text-lg font-semibold text-gray-600">Language</Text>
-                <ChevronRightIcon size={24} color="rgb(75, 85, 99)" />
+                {/* <ChevronRightIcon size={24} color="rgb(75, 85, 99)" /> */}
+                <View className="w-7 self-center">
+                <Image source={ngn} className="w-7 h-4" />
+                </View>
             </TouchableOpacity>
             <TouchableOpacity className="flex flex-row h-16 p-2 space-x-3 items-center">
                 <View className="bg-slate-300  rounded-lg  p-1">
                     <ArrowRightOnRectangleIcon size={23} color="#000" />
                 </View>
                 <Text className="flex-1 text-lg font-semibold text-gray-600">Logout</Text>
-                <ChevronRightIcon size={24} color="rgb(75, 85, 99)" />
             </TouchableOpacity>
             
     </SafeAreaView>
