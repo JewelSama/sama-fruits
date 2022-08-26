39ff541e05a43ae35a5f27f321189ebc8dba973b
Jewel Osafile <osafilejewel@gmail.com>
2022-08-26 15:12:08 +0100

    profilescreen

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -23,11 +23,11 @@ const ProfileScreen = () => {
     <ScrollView>
     <SafeAreaView className="p-5" style={GlobalStyles.droidSafeArea}>
       <View className="flex-row items-center space-x-20  justify-between">
-            <TouchableOpacity onPress={() => navigation.goBack()} className="bg-lightishPink rounded p-2">
+            <TouchableOpacity onPress={() => navigation.goBack()} className="bg-lightishPink rounded-lg p-2">
                 <ChevronLeftIcon size={25} style={{fontWeight: 'bold'}} color="#de1f27" />
             </TouchableOpacity>
             <Text className="text-2xl text-center font-bold">Profile</Text>
-            <TouchableOpacity className="bg-lightishPink rounded p-2">
+            <TouchableOpacity className="bg-lightishPink rounded-lg p-2">
                 <PencilSquareIcon size={25} style={{fontWeight: 'bold'}} color="#de1f27" />
             </TouchableOpacity>
             
