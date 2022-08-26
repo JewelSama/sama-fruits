ab295aa603094b5f3777a3c1549124a104750454
Jewel Osafile <osafilejewel@gmail.com>
2022-08-26 16:43:48 +0100

    Tab Navigator

--- App.js
+++ App.js
@@ -2,22 +2,31 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import {NavigationContainer} from '@react-navigation/native'
 import HomeScreen from './screens/HomeScreen';
 import ProfileScreen from './screens/ProfileScreen';
 
 export default function App() {
-
+  const Tab = createBottomTabNavigator()
   const Stack = createNativeStackNavigator()
 
+
+  const HomeTabs = () => {
+    return (
+      <Tab.Navigator screenOptions={{headerShown: false}}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        </Tab.Navigator>
+    );
+  };
 
   return (
     <NavigationContainer>
       <TailwindProvider>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={HomeTabs} />
           <Stack.Screen name="Profile" component={ProfileScreen} />
-        </Stack.Navigator>
+      </Stack.Navigator>
       </TailwindProvider>
     </NavigationContainer>
   );
