c8f19b6ca15af18113f85430c03e47c5e6db1554
Jewel Osafile <osafilejewel@gmail.com>
2022-09-06 12:54:13 +0100

    preparingOrderScreen

---
+++ screens/PreparingOrderScreen.jsx
@@ -0,0 +1,37 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React, {useEffect} from 'react'
+import GlobalStyles from '../GlobalStyles'
+import deliveryLoading from '../assets/deliveryLoading.gif'
+import { useNavigation } from '@react-navigation/native'
+import * as Animatable from 'react-native-animatable'
+import * as Progress from 'react-native-progress'
+
+
+const PreparingOrderScreen = () => {
+    const navigation  = useNavigation()
+    useEffect(() => {
+        setTimeout(() => {
+            navigation.navigate('Delivery')
+        }, 5000)
+    }, [])
+  return (
+    <SafeAreaView className="bg-[#00ccbb] flex-1 justify-center items-center" style={GlobalStyles.droidSafeArea}>
+      <Animatable.Image 
+        source={deliveryLoading}
+        animation="slideInUp"
+        iterationCount={1}
+        className="h-96 w-96 my-10"
+      />
+      <Animatable.Text
+        animation="slideInUp"
+        iterationCount={1}
+        className="text-lg text-white font-bold text-center"
+      >
+        Waiting for Restaurant tom accept your order!
+      </Animatable.Text>
+      <Progress.Circle size={60} indeterminate={true} color="white" />
+    </SafeAreaView>
+  )
+}
+
+export default PreparingOrderScreen
